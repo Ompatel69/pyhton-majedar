2912bd2d8de3b62fbf21ae1bb5c55fd478be3741
Ompatel69 <ompatel297676@gmail.com>
2024-04-28 11:13:21 +0530

    writelinesupdated

--- day50.py
+++ day50.py
@@ -12,6 +12,11 @@ while True:
     print(f"marks of student {i} in Maths is {m1}")
     print(f"marks of student {i} in English {m2}")
     print(f"marks of student {i} is SST is {m3}")
-    print(line)
+    print(line,"\n")
+    
 
 #yeh hai write line methods
+f = open("myfile2.txt", "w")
+print(f"line1\nline2\nline3\n")
+f.writelines(line)
+f.close()
